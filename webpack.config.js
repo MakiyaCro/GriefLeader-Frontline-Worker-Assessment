f7cf75577bb0192ef2ac1240251a7ce9216702ab
Makiya Crochiere <crochieremakiya@gmail.com>
2025-01-21 09:31:39 -0600

    Updated webpack config and added index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'baseapp/static/js/admin-dashboard.js'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'baseapp/static/dist'),
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   module: {
     rules: [
@@ -20,6 +22,11 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
   resolve: {
     extensions: ['.js', '.jsx']
   }
