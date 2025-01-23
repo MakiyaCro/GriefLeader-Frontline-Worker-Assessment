6b29e9d9ada2b96c5e771cede3748caac4bc84b5
Makiya Crochiere <crochieremakiya@gmail.com>
2025-01-23 12:46:23 -0600

    updated email functionality to use heroku sendgrid

--- FrontLWAA/settings.py
+++ FrontLWAA/settings.py
@@ -123,13 +123,12 @@ LOGIN_REDIRECT_URL = 'baseapp:dashboard'
 LOGOUT_REDIRECT_URL = 'baseapp:login'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 25
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = 'crochieremakiya@gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+DEFAULT_FROM_EMAIL = 'your-verified-sender@yourdomain.com'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
